3324b3b777580e5063c6dcd26e854af80ce858c2
Zach Brown <21brownz@da.org>
2019-08-28 08:20:39 -0400

    ya

---
+++ src/Unit1/SimpleIOMath.java
@@ -0,0 +1,19 @@
+package Unit1;
+
+import java.util.Scanner;
+
+public class SimpleIOMath {
+
+    public static void promptUser(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("* sit yourself down, take a seat *");
+        System.out.println("* All you gotta do is repeat after me *");
+        System.out.println("What is your name?");
+    }
+    public static void printInfo(){
+
+    }
+    public static void main(String[] args) {
+         promptUser();
+    }
+}
