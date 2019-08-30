0da8f31481a47a817331212aaa08984ca15a64fc
Zach Brown <21brownz@da.org>
2019-08-30 13:31:07 -0400

    fixed print vs println

--- src/Unit1/HolyGrail.java
+++ src/Unit1/HolyGrail.java
@@ -19,11 +19,11 @@ public class HolyGrail {
         Scanner scan = new Scanner(System.in);
         System.out.println("* A chat with a bridge keeper *");
         System.out.println("Who would cross the Bridge of Death must answer me these questions three, ere the other side he see.");
-        System.out.println("What is your name?");
+        System.out.print("What is your name? ");
             String name = scan.nextLine();
-        System.out.println("What is your quest?");
+        System.out.print("What is your quest? ");
             String quest = scan.nextLine();
-        System.out.println("What is your favorite color?");
+        System.out.print("What is your favorite color? ");
             String favcolor = scan.nextLine();
         System.out.println("King Arthur says, \"You have to know these things when you're a king, you know.\"");
         System.out.println("Your name is: " +name);
