e04b000c37094a469becb7bfa88355c837f075bc
Zach Brown <21brownz@da.org>
2019-10-04 12:04:44 -0400

    finished BMICalc lab

---
+++ src/Unit2/BMICalculator.java
@@ -0,0 +1,47 @@
+
+package Unit2;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ * A class that calculates a person's BMI
+ * @author 21brownz
+ * @version 1.0
+ */
+public class BMICalculator {
+
+    /** Convert English to metric units, perform the BMI calculation.
+     * NOTE: this method must properly handle bad data
+     * @param in height in meters
+     * @param lbs weight in kilograms
+     * @return the user's BMI expressed in kg/m^2 */
+
+    public static double computeBMI(int in, int lbs){
+        double m = in * 0.0254;
+        double kg = lbs * 0.454;
+        if(kg <= 0 || m <= 0){
+            return 0;
+        }
+        return kg/(m*m);
+    }
+
+    /**
+     * The entry point of application.
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your height in feet and inches (Ex 6'1\"):");
+        String htStr = scan.nextLine();
+        System.out.println("enter your weight in pounds:");
+        String wtStr = scan.nextLine();
+
+        int feet = Integer.parseInt(htStr.substring(0, htStr.indexOf("\'")));
+        int in = Integer.parseInt(htStr.substring(htStr.indexOf("\'")+1, htStr.indexOf("\"")));
+        int weight = Integer.parseInt(wtStr);
+        in += feet*12;
+        System.out.println("Your BMI, expressed as weight(kg)/height(m)^2: " + decimalFormat.format(computeBMI(in, weight)));
+    }
+}
--- src/Unit2/Chapter5.java
+++ src/Unit2/Chapter5.java
@@ -1,4 +1,31 @@
 package Unit2;
 
 public class Chapter5 {
-}
+    public static void main(String[] args) {
+        //declaration means name and type of a variable
+        //initialization means giving it a value
+        int a = 10, b , c = 81, d = 91, e;
+        int hours = 2; double pay = 12.75;
+        //to initialize an object, call a new keyword
+        String yourMom = "abracadabra";
+        String yourMom2 = "abracadabra";
+        System.out.println(yourMom == yourMom2);
+
+        int max = 1000;
+        int min = 60;
+        int range = max - min + 1;
+        int num = (int) (Math.random() * range) + min;
+
+        int miles = 98, gallons = 5;
+        double gasMileage = miles / gallons;
+        System.out.println(gasMileage);
+        }
+    public static int swapOnesAndTens(int num) {
+        int ones = num%10;
+        num /= 10;
+        int tens = num%100;
+        num /= 10;
+        return num*100 + ones*10 + tens;
+
+    }
+    }
