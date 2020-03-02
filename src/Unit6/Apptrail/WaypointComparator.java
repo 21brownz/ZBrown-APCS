597de8ac843141c20fa241a7802f95c7fea3919a
Zach Brown <21brownz@da.org>
2020-03-02 09:42:03 -0500

    revised WaypointComparator

--- src/Unit6/Apptrail/WaypointComparator.java
+++ src/Unit6/Apptrail/WaypointComparator.java
@@ -1,50 +1,34 @@
 package Unit6.Apptrail;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.Comparator;
 
 public class WaypointComparator implements Comparator<Waypoint> {
 
-    private String sort;
+    private int sort; // 0 - name; 1 - distance to springer; 2 - distance to katahdin; 3 - elevation
     private boolean asc;
 
-    public WaypointComparator(){
+    public WaypointComparator() {
         asc = true;
-        sort = "NA";
+        sort = 0;
     }
 
-    public WaypointComparator(boolean a, @NotNull String sort){
+    public WaypointComparator(int sort, boolean a) {
         this.asc = a;
-        this.sort = sort.toUpperCase();
+        this.sort = sort;
     }
 
-    @Override
     public int compare(Waypoint w1, Waypoint w2) {
-        if(asc) {
-            if ("NA".equals(sort)) {
-                return w1.getName().compareTo(w2.getName());
-            } else if ("DS".equals(sort)) {
-                return Double.compare(w1.getDistanceToSpringer(), w2.getDistanceToSpringer());
-            } else if ("DK".equals(sort)) {
-                return Double.compare(w1.getDistancetoKatahdin(), w2.getDistancetoKatahdin());
-            } else if ("EL".equals(sort)) {
-                return Integer.compare(w1.getElevation(), w2.getElevation());
-            } else {
-                throw new IllegalStateException("Illegal Waypoint");
-            }
-        }else{
-            if ("NA".equals(sort)) {
-                return w2.getName().compareTo(w1.getName());
-            } else if ("DS".equals(sort)) {
-                return Double.compare(w2.getDistanceToSpringer(), w1.getDistanceToSpringer());
-            } else if ("DK".equals(sort)) {
-                return Double.compare(w2.getDistancetoKatahdin(), w1.getDistancetoKatahdin());
-            } else if ("EL".equals(sort)) {
-                return Integer.compare(w2.getElevation(), w1.getElevation());
-            } else {
-                throw new IllegalStateException("Illegal Waypoint");
-            }
+        if(sort > 3 || sort < 0) throw new IllegalArgumentException("Invalid Input");
+        int diff = 0;
+        if (sort == 0) {
+            diff = w1.getName().compareTo(w2.getName());
+        } else if (sort == 1) {
+            diff = Double.compare(w1.getDistanceToSpringer(), w2.getDistanceToSpringer());
+        } else if (sort == 2) {
+            diff = Double.compare(w1.getDistancetoKatahdin(), w2.getDistancetoKatahdin());
+        } else {
+            diff = Integer.compare(w1.getElevation(), w2.getElevation());
         }
+        return asc ? diff : -diff;
     }
 }
